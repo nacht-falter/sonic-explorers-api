b6a478be7920128820b93a2b952133677b4d9575
nacht-falter <johannes.bernet@posteo.de>
2023-09-14 17:44:52 +0200

    feat: add signal for automatic user profile creation (#14)
    Add signal to automatically create a user profile when
    a new user is created.

--- profiles/apps.py
+++ profiles/apps.py
@@ -2,5 +2,10 @@ from django.apps import AppConfig
 
 
 class ProfilesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'profiles'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "profiles"
+
+    # Instructions for importing signals:
+    # https://www.geeksforgeeks.org/how-to-create-and-use-signals-in-django/
+    def ready(self):
+        import profiles.signals
