8952e673d446693bd4e2ac3194e189cfa44914af
nacht-falter <johannes.bernet@posteo.de>
2023-09-15 23:20:49 +0200

    test: add unit tests for SoundDetail view
    Add tests for SoundDetail view to sounds/tests.py

--- sounds/tests.py
+++ sounds/tests.py
@@ -66,3 +66,71 @@ class SoundListViewTests(APITestCase):
             "Unsupported video format or file",
             response.data["audio_file"]["detail"],
         )
+
+
+class SoundDetailViewTests(APITestCase):
+    """Tests for SoundDetail view"""
+
+    def setUp(self):
+        self.user1 = User.objects.create_user(
+            username="testuser1",
+            password="testpassword",
+        )
+        self.user2 = User.objects.create_user(
+            username="testuser2",
+            password="testpassword",
+        )
+        self.sound = Sound.objects.create(
+            owner=self.user1,
+            title="testtitle",
+            description="testdescription",
+            latitude=0.0,
+            longitude=0.0,
+        )
+        self.data = {
+            "title": "changedtitle",
+            "description": "changeddescription",
+            "latitude": 1.0,
+            "longitude": 1.0,
+        }
+
+        # set URL for sound detail view (instructions from:
+        # https://docs.djangoproject.com/en/4.2/ref/urlresolvers/#reverse)
+        self.url = reverse("sound_detail", args=[self.sound.id])
+
+        self.client = APIClient()
+
+        print(str(f"\n{self.id()}"))
+
+    def test_get_valid_sound(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["title"], self.sound.title)
+
+    def test_get_invalid_sound(self):
+        url = reverse("sound_detail", args=[2])
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_authenticated_user_can_update_owned_sound(self):
+        self.client.login(username="testuser1", password="testpassword")
+        response = self.client.put(self.url, self.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["title"], self.data["title"])
+
+    def test_user_cannot_upddate_another_users_sound(self):
+        self.client.login(username="testuser2", password="testpassword")
+        response = self.client.put(self.url, self.data)
+        sound = Sound.objects.get(id=self.sound.id)
+        self.assertNotEqual(sound.title, self.data["title"])
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_user_can_delete_owned_sound(self):
+        self.client.login(username="testuser1", password="testpassword")
+        response = self.client.delete(self.url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_user_cannot_delete_unowned_sound(self):
+        self.client.login(username="testuser2", password="testpassword")
+        response = self.client.delete(self.url)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
