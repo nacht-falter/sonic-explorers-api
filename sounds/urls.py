284822fbb2d058a74ae36c9c3e55214979365e40
nacht-falter <johannes.bernet@posteo.de>
2023-09-16 14:12:01 +0200

    feat: add tag list and detail views (#17)
    Add TagList and TagDetail views to sounds/views.py
    in order to  provide endpoints for listing and
    retrieving tags. Register tag urls.

--- sounds/urls.py
+++ sounds/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path("sounds/", views.SoundList.as_view(), name="sounds"),
     path("sounds/<int:pk>", views.SoundDetail.as_view(), name="sound_detail"),
+    path("tags/", views.TagList.as_view(), name="tags"),
+    path("tags/<int:pk>", views.TagDetail.as_view(), name="tag_detail"),
 ]
--- sounds/views.py
+++ sounds/views.py
@@ -1,11 +1,12 @@
 from rest_framework import generics, permissions
 from .models import Sound
-from .serializers import SoundSerializer, SoundDetailSerializer
+from .serializers import SoundSerializer, SoundDetailSerializer, TagSerializer
 from sonic_explorers_api.permissions import IsOwnerOrReadOnly
 
 
 class SoundList(generics.ListCreateAPIView):
     """Lists all sounds or creates a new sound."""
+
     queryset = Sound.objects.all()
     serializer_class = SoundSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
@@ -16,6 +17,21 @@ class SoundList(generics.ListCreateAPIView):
 
 class SoundDetail(generics.RetrieveUpdateDestroyAPIView):
     """Retrieves, updates, or deletes a sound."""
+
     queryset = Sound.objects.all()
     serializer_class = SoundDetailSerializer
     permission_classes = [IsOwnerOrReadOnly]
+
+
+class TagList(generics.ListAPIView):
+    """Lists all tags."""
+
+    queryset = Sound.tags.tag_model.objects.all()
+    serializer_class = TagSerializer
+
+
+class TagDetail(generics.RetrieveAPIView):
+    """Retrieves a tag."""
+
+    queryset = Sound.tags.tag_model.objects.all()
+    serializer_class = TagSerializer
