69cd101d647ddb940ef37369fe5a927bbcb513d2
nacht-falter <johannes.bernet@posteo.de>
2023-09-19 23:46:33 +0200

    fix: fix drf datetime format string in settings.py

--- sonic_explorers_api/settings.py
+++ sonic_explorers_api/settings.py
@@ -36,7 +36,7 @@ REST_FRAMEWORK = {
     ],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 10,
-    "DATETIME_FORMAT": "%d %b %Y, I:%M %p",
+    "DATETIME_FORMAT": "%d %b %Y, %I:%M %p",
 }
 if not development:
     REST_FRAMEWORK["DEFAULT_RENDERER_CLASSES"] = [
