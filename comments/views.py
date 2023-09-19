4205756949f2d651aced3a4bab6a043b8cbb4ec9
nacht-falter <johannes.bernet@posteo.de>
2023-09-19 11:41:00 +0200

    feat: add filters to comment endpoints (#18)
    Add filtering and sorting options to comment endpoints
    for filtering comments by sound and sorting them by
    creation and update time.

--- comments/views.py
+++ comments/views.py
@@ -1,8 +1,30 @@
-from rest_framework import generics, permissions
-from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import generics, permissions, filters
+from django_filters import rest_framework as drf_filters
 from sonic_explorers_api.permissions import IsOwnerOrReadOnly
 from .models import Comment
+from sounds.models import Sound
 from .serializers import CommentSerializer
+
+
+class CommentFilter(drf_filters.FilterSet):
+    """Custom filter for comment list view. Provides filtering options to
+    filter comments by sound.
+
+    Instructions from django-filter docs:
+    https://django-filter.readthedocs.io/en/main/guide/rest_framework.html
+    """
+
+    queryset = Sound.objects.all()
+
+    comments_by_sound = drf_filters.ModelChoiceFilter(
+        queryset=queryset,
+        field_name="sound",
+        label="Show comments for sound:",
+    )
+
+    class Meta:
+        model = Comment
+        fields = ["comments_by_sound"]
 
 
 class CommentList(generics.ListCreateAPIView):
@@ -11,13 +33,19 @@ class CommentList(generics.ListCreateAPIView):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    filter_backends = [
+        filters.OrderingFilter,
+        drf_filters.DjangoFilterBackend,
+    ]
+    ordering_fields = ["created_at", "updated_at"]
+    filterset_class = CommentFilter
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
 
 class CommentDetail(generics.RetrieveUpdateDestroyAPIView):
-    """Retrieve a comment. Update or delete comments if user is logged in."""
+    """Retrieve a comment. Update or delete comments if user is owner."""
 
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
