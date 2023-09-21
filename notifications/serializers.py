141f5541c13834bc86970d492c12ad131bed1cd5
nacht-falter <johannes.bernet@posteo.de>
2023-09-21 16:52:07 +0200

    fix: remove broken fields in notification serializer
    Remove broken ReadOnlyFields for category, title, and
    content in NotificationSerializer.

--- notifications/serializers.py
+++ notifications/serializers.py
@@ -9,16 +9,14 @@ class NotificationSerializer(serializers.ModelSerializer):
     """Serializer for Notification model. Includes method for formatting
     sent_at field for better readability.
     """
+
     owner = serializers.ReadOnlyField(source="owner.username")
     sender = serializers.ReadOnlyField(source="sender.username")
     sender_avatar = serializers.ReadOnlyField(
         source="sender.profile.avatar.url"
     )
     item_id = serializers.ReadOnlyField(source="item.id")
-    title = serializers.ReadOnlyField(source="item.title")
-    content = serializers.ReadOnlyField(source="item.content")
     sent_at = serializers.SerializerMethodField()
-
 
     def get_sent_at(self, obj):
         if obj.sent_at > timezone.now() - timedelta(days=1):
@@ -38,4 +36,5 @@ class NotificationSerializer(serializers.ModelSerializer):
             "is_read",
             "title",
             "content",
+            "category",
         ]
