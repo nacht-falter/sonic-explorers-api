7cc63b7ce894aa0782ec1c524fa4dd065072bd22
nacht-falter <johannes.bernet@posteo.de>
2023-09-21 20:11:58 +0200

    fix: remove trailing slash from report_detail url

--- profiles/urls.py
+++ profiles/urls.py
@@ -4,7 +4,7 @@ from profiles import views
 urlpatterns = [
     path("profiles/", views.ProfileList.as_view(), name="profiles"),
     path(
-        "profiles/<int:pk>/",
+        "profiles/<int:pk>",
         views.ProfileDetail.as_view(),
         name="profile_detail",
     ),
